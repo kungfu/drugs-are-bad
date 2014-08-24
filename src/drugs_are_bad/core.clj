f0f6a467027f80a314cba5546cf91a30b1306ec9
Chad Gilbert <chad.gilbert@cqlcorp.com>
2014-08-24 10:04:54 -0400

    Comments and clean-up in prep for http server inclusion

--- src/drugs_are_bad/core.clj
+++ src/drugs_are_bad/core.clj
@@ -1,19 +1,25 @@
 (ns drugs-are-bad.core
   (:gen-class))
 
+; Sorts a murder of dolls by their weight, ascending
 (defn weight-sort [ds] 
   (sort-by #(:weight %) ds))
 
+; Sorts a murder of dolls by their name, ascending
 (defn name-sort [ds]
   (sort-by #(:name %) ds))
 
+; Calculates the total value of a murder of dolls
 (defn valsum [dolls] 
   (reduce + (map #(:value %) dolls)))
 
+; Calculates the most valuable murder of dolls, given multiple.
 (defn max-murder-of-dolls [murder]
   (last (sort-by valsum murder))) 
 
-(defn burden-sorted
+; Figures out how to best burden the mule, assuming the dolls are sorted by weight. 
+; (Private because of the sorting assumption)
+(defn- burden-sorted
   ([mw dolls] 
     (let [d (first dolls)
           dw (:weight d)]
@@ -23,13 +29,7 @@
           (cons d (burden-sorted (- mw dw) (rest dolls)))])
         []))))
 
+; Figures out how to best burden the mule, given a max weight and a murder of dolls
 (defn burden [mw dolls]
   (burden-sorted mw (weight-sort dolls)))
-
-
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello world"))
 
